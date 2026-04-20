fa286529d5c1ef38f97cb23776b171bc0b9f0b6f
Davi <davisantosnavega06@gmail.com>
2026-04-20 18:17:55 -0300

    Refactor alterar.php for question update functionality

--- alterar.php
+++ alterar.php
@@ -1,12 +1,40 @@
 <?php
-$idAlterar = $_GET['id'];
-$linhas = file("perguntas.txt");
-$perguntaEncontrada = 0;
+$id = $_GET['id'];
+$arquivo = "perguntas.txt";
+$linhas = file($arquivo);
+$atual = [];
 
-while (!feof) {
-    $dados = explode(" ; ", $linha);
-    if ($dados[0] == $idAlterar) {
-        $perguntaEncontrada = $dados;
+foreach ($linhas as $linha) {
+    $dados=explode(";", trim($linha));
+    if ($dados[0]==$id) {
+        $atual=$dados;
         break;
     }
 }
+
+if (isset($_POST['salvar'])) {
+    $novaLinha=$id . ";" . $_POST['tipo'] . ";" . $_POST['enunciado'] . ";" . $_POST['respostas'] . PHP_EOL;
+    $txt="";
+
+    foreach ($linhas as $linha) {
+        $dados=explode(";", trim($linha));
+        
+        if ($dados[0]==$id) {
+            $txt=$txt . $novaLinha;
+        } else { 
+            $txt=$txt . $linha;
+        }
+    }
+
+    file_put_contents($arquivo, $txt);
+    header("Location: listar.php");
+}
+?>
+
+<form method="POST">
+    <h3>Alterar Pergunta <?php echo $id; ?></h3>
+    <input type="text" name="tipo" value="<?php echo $atual[1]; ?>"><br>
+    <input type="text" name="enunciado" value="<?php echo $atual[2]; ?>"><br>
+    Respostas: <input type="text" name="respostas" value="<?php echo $atual[3]; ?>"><br>
+    <button type="submit" name="salvar">Atualizar</button>
+</form>
